d61f777eedc79fc07d472e15d330322cb6063b83
ako652 <akotabe0213@gmail.com>
2023-04-26 15:40:28 +0200

    react-basics

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+
+export default function Home(){
+  return  <div className="home">
+    <div className="content">
+        <h1 id="red">Sale 20% off</h1>
+        <h1>On everything</h1>
+        <p>discover the secrets to glowing, radiant skin with 
+            our carefully curated collection
+        </p>
+        <button>show now</button>
+        </div>
+    </div>
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import logo from "../images/logo.png"
+
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+ return <div className="navbar"> 
+      <div id="logo">
+            <img src={logo} alt="logo" />
+          </div> 
+        <ul>
+          <li>Home</li>
+           <li>products</li>
+           <li>favorite</li>
+           <li>Cart</li>
+
+            
+          </ul> 
+          
+
+         </div>;
 }
